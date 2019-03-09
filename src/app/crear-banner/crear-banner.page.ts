e806b8220475688a40b0d4bf64bb673c44436981
eliab51 <eliabjselvacruz51@gmail.com>
2019-03-09 13:25:36 -0600

    Un motón de cambios, porque se me había olvidado dar commit y push, jjj...saludes

--- src/app/crear-banner/crear-banner.page.ts
+++ src/app/crear-banner/crear-banner.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild} from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-crear-banner',
@@ -7,9 +8,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrearBannerPage implements OnInit {
 
-  constructor() { }
+  @ViewChild('canvas') canvasEl : ElementRef;
+  private _CANVAS  : any;
+  private _CONTEXT : any;
+  private _IMG : any;
 
+  constructor(public navCtrl: NavController)
+  {
+
+  }
   ngOnInit() {
+    this._CANVAS 	      = this.canvasEl.nativeElement;
+    this._CANVAS.width  	= 200;
+    this._CANVAS.height 	= 200;
+    this._IMG = new Image();
+    this._IMG.src = 'https://pbs.twimg.com/profile_images/614117729918914560/3VZw4vhK_400x400.jpg';
+    this.initialiseCanvas();
+  }
+
+  initialiseCanvas():void{
+    if(this._CANVAS.getContext)
+    {
+        this.setupCanvas();
+    }
+  }
+
+  drawCircle():void{
+    this.clearCanvas();
+    this._CONTEXT.beginPath();
+  
+    // x, y, radius, startAngle, endAngle
+    this._CONTEXT.drawImage(this._IMG,0,0,200,200);
+    this._CONTEXT.font = '20px Arial';
+    this._CONTEXT.fillStyle = '#fff';
+    this._CONTEXT.fillText('Eliab Selva', 30, 120);
+    this._CONTEXT.fillText('Favio López', 30, 140);
+    this._CONTEXT.fillText('Klissman Espinoza', 30, 160);
+  }
+
+  setupCanvas():void{
+    this._CONTEXT = this._CANVAS.getContext('2d');
+    this._CONTEXT.fillStyle = "#3e3e3e";
+    this._CONTEXT.fillRect(0, 0, 200, 200);
+  }
+
+  clearCanvas(){
+    this._CONTEXT.clearRect(0, 0, this._CANVAS.width, this._CANVAS.height);
+    this.setupCanvas();
   }
 
 }
