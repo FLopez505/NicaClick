af2641e760093a65caba7a3c6124134d4a23c999
eliab51 <eliabjselvacruz51@gmail.com>
2019-03-26 19:46:27 -0600

    Control de dimenciones de imagenes cargadas en el canvas

--- src/app/crear-banner/crear-banner.page.ts
+++ src/app/crear-banner/crear-banner.page.ts
@@ -16,6 +16,8 @@ export class CrearBannerPage implements OnInit {
   private _CANVAS  : any;
   private _CONTEXT : any;
   private _IMG : any;
+  private _width : any;
+  private _height : any;
 
   plantillaCargada: plantilla;
 
@@ -33,10 +35,12 @@ export class CrearBannerPage implements OnInit {
       this.plantillaCargada = this.misplantillasservice.obtenerunicaPlantilla(plantillaId);
     });
     this._CANVAS 	      = this.canvasEl.nativeElement;
-    this._CANVAS.width  	= 851;
-    this._CANVAS.height 	= 315;
+    this._width = this.plantillaCargada.widht;
+    this._height = this.plantillaCargada.height;
+    this._CANVAS.width  	= this._width;
+    this._CANVAS.height 	= this._height;
     this._IMG = new Image();
-    this._IMG.src = this.plantillaCargada.imgSrc; //'https://pbs.twimg.com/profile_images/614117729918914560/3VZw4vhK_400x400.jpg';
+    this._IMG.src = this.plantillaCargada.imgSrc;
     this.initialiseCanvas();
     console.log(this.plantillaCargada.imgSrc);
     console.log(this._IMG.src);
@@ -49,28 +53,26 @@ export class CrearBannerPage implements OnInit {
     }
   }
 
-  drawCircle():void{
+ //Metodo para crear el canva
+  drawCanva():void{
     this.clearCanvas();
     this._CONTEXT.beginPath();
-  
-    // x, y, radius, startAngle, endAngle
-    this._CONTEXT.drawImage(this._IMG,0,0,851,315);
-    this._CONTEXT.font = '20px Arial';
+    this._CONTEXT.drawImage(this._IMG,0,0,this._width,this._height);
+    this._CONTEXT.font = '30px Arial';
     this._CONTEXT.fillStyle = '#137e73';
-    this._CONTEXT.fillText('Eliab Selva', 30, 120);
-    this._CONTEXT.fillText('Favio López', 30, 140);
-    
+    this._CONTEXT.fillText('Eliab Selva', 30, 100);
+    this._CONTEXT.fillText('Favio López', 30, 130);
     this._CONTEXT.fillText('Klissman Espinoza', 30, 160);
   }
 
   setupCanvas():void{
     this._CONTEXT = this._CANVAS.getContext('2d');
-    this._CONTEXT.fillStyle = "#3e3e3e";
-    this._CONTEXT.fillRect(0, 0, 851, 315);
+    this._CONTEXT.beginPath();
+    this._CONTEXT.drawImage(this._IMG,0,0,this._width,this._height);
   }
 
   clearCanvas(){
-    this._CONTEXT.clearRect(0, 0, this._CANVAS.width, this._CANVAS.height);
+    this._CONTEXT.clearRect(0, 0, this._width, this._height);
     this.setupCanvas();
   }
 
